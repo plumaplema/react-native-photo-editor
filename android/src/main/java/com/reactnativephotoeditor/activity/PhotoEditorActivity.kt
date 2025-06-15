524fcc4195545999183c298f30360ce8ab24cae9
plumaplema <plumaplema@gmail.com>
2025-06-16 00:45:57 +0800

    feat(ui): enable immersive sticky mode for fullscreen experience

--- android/src/main/java/com/reactnativephotoeditor/activity/PhotoEditorActivity.kt
+++ android/src/main/java/com/reactnativephotoeditor/activity/PhotoEditorActivity.kt
@@ -182,6 +182,14 @@ open class PhotoEditorActivity : AppCompatActivity(), OnPhotoEditorListener, Vie
       WindowManager.LayoutParams.FLAG_FULLSCREEN,
       WindowManager.LayoutParams.FLAG_FULLSCREEN
     )
+    window.decorView.systemUiVisibility = (
+      View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+      or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+      or View.SYSTEM_UI_FLAG_FULLSCREEN
+      or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+      or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+      or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+    )
   }
 
   private fun initViews() {
